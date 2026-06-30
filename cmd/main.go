96c41eb77944abd911d77ebabac410b2ee0ea0ad
rezafauzan <rezafauzan593@gmail.com>
2026-06-30 11:55:15 +0700

    Feat: add randomize interval

--- cmd/main.go
+++ cmd/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"log"
+	"math/rand/v2"
 	"rezafauzan/latihan-rabbitmq/internal/lib"
+	"time"
 )
 
 func main() {
@@ -10,15 +12,15 @@ func main() {
 	if err != nil {
 		log.Fatal("Rabbit Fail To Establish")
 	}
-
-	err = lib.ProduceMessage(rabbit.RMQ, "dev.test", "ini itu")
-	if err != nil {
-		log.Fatal("Rabbit Fail To Produce Message", err)
+	
+	for true {
+		err = lib.ProduceMessage(rabbit.RMQ, "dev.test", "ini itu")
+		if err != nil {
+			log.Fatal("Rabbit Fail To Produce Message", err)
+		}
+	
+		log.Printf("OK")
+		time.Sleep(time.Duration(rand.Float64()) * time.Second)
 	}
 
-	err = lib.ProduceMessage(rabbit.RMQ, "dev.test", "ini itu")
-	if err != nil {
-		log.Fatal("Rabbit Fail To Produce Message", err)
-	}
-	log.Printf("OK")
 }
